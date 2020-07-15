e8fafb34221dd78b0a4fea3018c99084ab259c01
Shaidee Alingcastre <shaidee@alingcastre.net>
2020-07-15 13:00:06 -0700

    Create capitalizeSentences.js

---
+++ capitalizeSentences.js
@@ -0,0 +1,35 @@
+/* 7kyu
+  In English, all words at the begining of a sentence should begin with a capital letter.
+
+  You will be given a paragraph that does not use capital letters. Your job is to capitalise the first letter of the first word of each sentence.
+
+  For example,
+
+  input:
+
+  "hello. my name is inigo montoya. you killed my father. prepare to die."
+
+  output:
+
+  "Hello. My name is inigo montoya. You killed my father. Prepare to die."
+
+  You may assume:
+
+  there will be no punctuation besides full stops and spaces
+
+  all but the last full stop will be followed by a space and at least one word
+*/
+
+function fix(paragraph){  
+  if (paragraph) {
+    return paragraph.split(". ")
+                    .map(word => word[0].toUpperCase() + word.substring(1))
+                    .join(". ");
+  }
+  
+  return paragraph;
+}
+
+console.log(fix("")) // return ""
+console.log(fix("hi.")) // return "Hi."
+console.log(fix("hello. my name is inigo montoya. you killed my father. prepare to die.")) // return "Hello. My name is inigo montoya. You killed my father. Prepare to die."
