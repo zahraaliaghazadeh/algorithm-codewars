d318e9ce7bdaa9914e4c49acce71f4ccfc5b0201
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-15 11:42:47 -0700

    array element parity

---
+++ ArrayElementParity/index.js
@@ -0,0 +1,35 @@
+// 7kyu
+// In this Kata, you will be given an array of integers whose elements have both a negative and a positive value, except for one integer that is either only negative or only positive. Your task will be to find that integer.
+
+// Examples:
+
+// [1, -1, 2, -2, 3] => 3
+
+// 3 has no matching negative appearance
+
+// [-3, 1, 2, 3, -1, -4, -2] => -4
+
+// -4 has no matching positive appearance
+
+// [1, -1, 2, -2, 3, 3] => 3
+
+// (the only-positive or only-negative integer may appear more than once)
+
+function solve(arr){
+    //..
+  for (let i=0;i<arr.length;i++){
+        if (!arr.includes(-arr[i])){
+        return arr[i]
+
+    }
+  }
+};
+
+
+
+
+console.log(solve([1,-1,2,-2,3]))                             // 3
+console.log(solve([-3,1,2,3,-1,-4,-2]))                       // -4
+console.log(solve([1,-1,2,-2,3,3]))                           // 3
+console.log(solve([-110,110,-38,-38,-62,62,-38,-38,-38]))     // -38
+console.log(solve([ -9,-105,-9,-9,-9,-9,105]))                // -9
