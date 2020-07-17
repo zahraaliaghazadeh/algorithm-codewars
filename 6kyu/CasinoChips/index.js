a0c45f14fe068663454cc85076ab3f7d3d45ff0b
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-17 14:50:02 -0700

    casino chips directions added

--- 6kyu/CasinoChips/index.js
+++ 6kyu/CasinoChips/index.js
@@ -1,5 +1,20 @@
 // 6kyu
 
+// You are given three piles of casino chips: white, green and black chips:
+
+// the first pile contains only white chips
+// the second pile contains only green chips
+// the third pile contains only black chips
+// Each day you take exactly two chips of different colors and head to the casino. You can chose any color, but you are not allowed to take two chips of the same color in a day.
+
+// You will be given an array representing the number of chips of each color and your task is to return the maximum number of days you can pick the chips. Each day you need to take exactly two chips.
+
+// solve([1,1,1]) = 1, because after you pick on day one, there will be only one chip left
+// solve([1,2,1]) = 2, you can pick twice; you pick two chips on day one then on day two
+// solve([4,1,1]) = 2
+// More examples in the test cases. Good luck!
+
+// Brute force is not the way to go here. Look for a simplifying mathematical approach.
 
 
 function solve(arr){
@@ -16,7 +31,14 @@ function solve(arr){
   }
 
 
-
+  console.log(solve([1,1,1]))     // 1
+  console.log(solve([1,2,1]))     // 2
+  console.log(solve([4,1,1]))     // 2
+  console.log(solve([8,2,8]))     // 9
+  console.log(solve([8,1,4]))     // 5
+  console.log(solve([7,4,10]))    // 10
+  console.log(solve([12,12,12]))  // 18 
+  console.log(solve([1,23,2]))    // 3
 
 
 // ========other solution=========
