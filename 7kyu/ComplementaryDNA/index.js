becd53c51a132747a27d0c19430a6ce8b299b27b
sali6798 <shaidee@alingcastre.net>
2020-07-21 13:31:16 -0700

    solution for comp DNA

---
+++ 7kyu/ComplementaryDNA/index.js
@@ -0,0 +1,41 @@
+/* 7kyu
+	Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the 
+	"instructions" for the development and functioning of living organisms.
+
+	If you want to know more http://en.wikipedia.org/wiki/DNA
+
+	In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". You have 
+	function with one side of the DNA (string, except for Haskell); you need to get the other 
+	complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+
+	More similar exercise are found here http://rosalind.info/problems/list-view/ (source)
+
+	DNAStrand ("ATTGC") // return "TAACG"
+
+	DNAStrand ("GTAT") // return "CATA" 
+
+*/
+
+function DNAStrand(dna){
+  const pairs = { A:'T', T:'A', C:'G', G:'C' };
+
+  return dna.replace(/./g, x => pairs[x]);
+}
+
+console.log(DNAStrand("AAAA")) 	// "TTTT"
+console.log(DNAStrand("ATTGC")) // "TAACG"
+console.log(DNAStrand("GTAT")) 	// "CATA"
+
+/*========== DIFFERENT SOLUTION ============
+function DNAStrand(dna){
+  const pairs = {
+    A : "T",
+    T : "A",
+    G : "C",
+    C : "G"
+  }
+  
+  return dna.split("").map(x => pairs[x]).join("");
+}
+
+*/
