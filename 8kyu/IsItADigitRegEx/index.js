9aaf935e5d2786ac420fc3077da999b9e520d4ef
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-22 11:21:04 -0700

    regular exp is it a digit

--- 8kyu/IsItADigitRegEx/index.js
+++ 8kyu/IsItADigitRegEx/index.js
@@ -1,23 +1,37 @@
 // 8kyu
 // Implement String#digit? (in Java StringUtils.isDigit(String)), which should return true if given object is a digit (0-9), false otherwise.
 
-
-
 String.prototype.digit = function() {
-    return /^\d$/.test(this);
+    return /^\d$/.test(this)
   };
 
+     console.log(''.digit())
+    //   false
+     console.log('7'.digit())
+    //   true
+     console.log(' '.digit())
+    //   false
+     console.log('a'.digit())
+    //   false
+     console.log('a5'.digit())
+    //   false
+     console.log('14'.digit())
+    //   false
 
-  String.prototype.digit = function() {
-    return /^[0-9]$/.test(this);
-  };
 
 
-  String.prototype.digit = function() {
-    return this.match(/^[0-9]$/) ? true : false;
-  };
 
+// other solution
+//   String.prototype.digit = function() {
+//     return /^[0-9]$/.test(this);
+//   };
 
-  String.prototype.digit = function() {
-    return this.length === 1 && '0' <= this && this <= '9';
-  };
+// other solution
+//   String.prototype.digit = function() {
+//     return this.match(/^[0-9]$/) ? true : false;
+//   };
+
+// other solution
+//   String.prototype.digit = function() {
+//     return this.length === 1 && '0' <= this && this <= '9';
+//   };
