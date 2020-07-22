46e09ca6e8da43a04542a9ad73093eaa6ab67d7a
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-22 13:18:54 -0700

    sum of numbers

--- 7kyu/SumOfNumbers/index.js
+++ 7kyu/SumOfNumbers/index.js
@@ -26,3 +26,25 @@ function getSum( a,b )
   }
   return sum;
 }
+
+
+
+       
+    console.log(getSum(0,-1))
+    // -1
+    console.log(getSum(0,1))
+    // 1
+    
+// other solution
+    // const GetSum = (a, b) => {
+    //     let min = Math.min(a, b),
+    //         max = Math.max(a, b);
+    //     return (max - min + 1) * (min + max) / 2;
+    //   }
+
+
+// other solution
+//       function GetSum(a,b)
+// {
+//   return (Math.abs(a - b) + 1) * (a+b) / 2;
+// }
