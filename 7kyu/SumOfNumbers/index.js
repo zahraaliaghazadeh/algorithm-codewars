d29cf5714a334a84e89d6570d94d797aa0ab584f
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-22 13:16:32 -0700

    sum of numbers

---
+++ 7kyu/SumOfNumbers/index.js
@@ -0,0 +1,28 @@
+// 7kyu
+// Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples
+// GetSum(1, 0) == 1   // 1 + 0 = 1
+// GetSum(1, 2) == 3   // 1 + 2 = 3
+// GetSum(0, 1) == 1   // 0 + 1 = 1
+// GetSum(1, 1) == 1   // 1 Since both are same
+// GetSum(-1, 0) == -1 // -1 + 0 = -1
+// GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+
+
+function getSum( a,b )
+{
+   if (a === b) {
+     return a;
+   }
+  const max = Math.max(a, b);
+  const min = Math.min(a, b);
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
