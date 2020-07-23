513df6f67902ba6e390e8d579b78fabb995aa204
sali6798 <shaidee@alingcastre.net>
2020-07-22 22:17:23 -0700

    solution for list filtering

---
+++ 7kyu/ListFiltering/index.js
@@ -0,0 +1,26 @@
+/* 7kyu
+In this kata you will create a function that takes a list of non-negative 
+integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+*/
+
+function filter_list(l) {
+    return l.filter(item => typeof item === "number");
+}
+
+console.log(filter_list([1,2,'a','b']))                 // returns [1,2]
+console.log(filter_list([1,'a','b',0,15]))              // returns [1,0,15]
+console.log(filter_list([1,2,'aasf','1','123',123]))    // returns [1,2,123]
+
+/*========== DIFFERENT SOLUTION ===========
+
+function filter_list(l) {
+  return l.filter(Number.isInteger);
+}
+
+*/
