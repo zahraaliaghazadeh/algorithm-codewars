0ad823ef6b0dfc164914bd14d7292499dd72bf3d
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-23 17:15:09 -0700

    count of pos , sum of neg

--- 8kyu/CountOfPosSumOfNeg/index.js
+++ 8kyu/CountOfPosSumOfNeg/index.js
@@ -1,7 +1,22 @@
 // 8kyu
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+// If the input array is empty or null, return an empty array.
+
+// Example
+// For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 
 
+
+// testData = [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14];
+// actual = countPositivesSumNegatives(testData);
+// expected = [8, -50];
+
+// Test.expect(actual[0] == expected[0] && actual[1] == expected[1], "Wrong return value.");
+// });
 
 
 function countPositivesSumNegatives(input) {
@@ -22,6 +37,7 @@ function countPositivesSumNegatives(input) {
     return [positive, negative];
 }
 
+    
 
 
 // other solution
