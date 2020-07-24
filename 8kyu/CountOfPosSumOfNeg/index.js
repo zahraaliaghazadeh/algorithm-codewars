18f08555e62051307a4866d44f00f98be57683cc
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-23 17:13:00 -0700

    count of positive , sum of negative

---
+++ 8kyu/CountOfPosSumOfNeg/index.js
@@ -0,0 +1,30 @@
+// 8kyu
+
+
+
+
+
+function countPositivesSumNegatives(input) {
+    if (input == null || input.length == 0)
+      return [];
+    
+    var positive = 0;
+    var negative = 0;
+    
+    for (var i=0, l=input.length; i<l; ++i)
+    {
+      if (input[i] > 0)
+        ++ positive;
+      else
+        negative += input[i];
+    }
+    
+    return [positive, negative];
+}
+
+
+
+// other solution
+function countPositivesSumNegatives(input) {
+    return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+}
