8fc7f118fbdeb31153c3ac3f445f98f4120bfd2f
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-25 23:45:24 -0700

    wip

--- 8kyu/xorLogicalOperator/index.js
+++ 8kyu/xorLogicalOperator/index.js
@@ -22,15 +22,13 @@ function xor(a, b) {
     }
   }
 
-describe("Your 'xor' function/operator", _ => {
-    it("should work for the four basic cases described above", _ => {
-      Test.assertEquals(xor(false, false), false, "false xor false === false");
-      Test.assertEquals(xor(true, false), true, "true xor false === true");
-      Test.assertEquals(xor(false, true), true, "false xor true === true");
-      Test.assertEquals(xor(true, true), false, "true xor true === false");
-      Test.assertNotEquals(xor(true, true), true, "'xor' is NOT identical to 'or'");
-    });
-  });
+
+      console.log(xor(false, false), false, "false xor false === false");
+      console.log(xor(true, false), true, "true xor false === true");
+      console.log(xor(false, true), true, "false xor true === true");
+      console.log(xor(true, true), false, "true xor true === false");
+      console.log(xor(true, true), true, "'xor' is NOT identical to 'or'");
+ 
 
 
   function xor(a, b) {
