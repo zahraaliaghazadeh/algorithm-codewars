ec3a95bd117af38e5c84de43dc38ac9ebfc84a4a
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-27 11:19:58 -0700

    wip

--- 8kyu/SortAndStar/index.js
+++ 8kyu/SortAndStar/index.js
@@ -20,3 +20,8 @@ function twoSort(s) {
     }
     return result 
   }
+
+  Test.describe("Basic tests",_=>{
+    Test.assertEquals(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
+    Test.assertEquals(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e'); 
+    });
