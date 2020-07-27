4d8f3932600da359094a05c9409af650602a1632
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-27 11:21:06 -0700

    wip

--- 8kyu/SortAndStar/index.js
+++ 8kyu/SortAndStar/index.js
@@ -21,13 +21,13 @@ function twoSort(s) {
     return result 
   }
 
-  Test.describe("Basic tests",_=>{
-    Test.assertEquals(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
-    Test.assertEquals(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e'); 
-    });
+
+   console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
+   console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e'); 
+ 
 
 
 // other solution
-    function twoSort(s) {
-        return s.sort()[0].split('').join('***');
-      }
+    // function twoSort(s) {
+    //     return s.sort()[0].split('').join('***');
+    //   }
