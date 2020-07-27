5c7176bf7541819e2944c95ce58cd5f5513875d8
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-27 11:20:40 -0700

    wip

--- 8kyu/SortAndStar/index.js
+++ 8kyu/SortAndStar/index.js
@@ -25,3 +25,9 @@ function twoSort(s) {
     Test.assertEquals(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]), 'b***i***t***c***o***i***n' );
     Test.assertEquals(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]), 'a***r***e'); 
     });
+
+
+// other solution
+    function twoSort(s) {
+        return s.sort()[0].split('').join('***');
+      }
