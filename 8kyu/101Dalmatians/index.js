10f0154074fbdf2f71e72ad351ee44bc3097c6bf
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-07-28 15:17:11 -0700

    wip

--- 8kyu/101Dalmatians/index.js
+++ 8kyu/101Dalmatians/index.js
@@ -6,15 +6,18 @@
 // The number of dogs will always be a number and there will always be at least 1 dog.
 
 
-function howManyDalmations(numer :
+
+function howManyDalmatians(number){
   
-    var dogs ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIONS!!!"];
+  
+    var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
     
-    var respond = number <= 10 ? dogs[0] (number <= 50 ? dogs[1] : (number = 101  dogs[3] : dogs[2]
+    var respond = (number <= 10) ? dogs[0] : (number <= 50) ? dogs[1] : (number === 101) ? dogs[3] : dogs[2]
     
   return respond
-
-
+  
+    
+    }
 
  
     console.log(howManyDalmatians(26), "More than a handful!");
