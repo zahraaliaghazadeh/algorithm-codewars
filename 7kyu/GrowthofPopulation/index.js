07edbad2ba4d8c1390f361220056e0e4f33bb9c1
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-01 23:57:07 -0700

    wip

--- 7kyu/GrowthofPopulation/index.js
+++ 7kyu/GrowthofPopulation/index.js
@@ -18,3 +18,16 @@ function nbYear(p0, percent, aug, p) {
   
   return years
 }
+
+
+
+       console.log(nbYear(1500, 5, 100, 5000), 15);
+       console.log(nbYear(1500000, 2.5, 10000, 2000000), 10);
+       console.log(nbYear(1500000, 0.25, 1000, 2000000), 94);
+
+
+    // other solution
+    // function nbYear(p0, percent, aug, p) {
+    //     for(var y = 0; p0 < p; y++) p0 = p0 * (1 + percent / 100) + aug;
+    //     return y;
+    //   }
