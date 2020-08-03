adb5ba8bb977990b9a336ead8b06e61adeb27207
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-03 12:47:07 -0700

    wip

--- 7kyu/FibonaccisFizzBuzz/index.js
+++ 7kyu/FibonaccisFizzBuzz/index.js
@@ -34,20 +34,15 @@
 
 
 
-describe ("Fibonacci's FizzBuzz", function() {
 
-    it ("should work for a value of 2", function() {
-        Test.assertSimilar(fibsFizzBuzz(2),[ 1, 1 ])
-    });
+        console.log(fibsFizzBuzz(2),[ 1, 1 ])
+
     
-    it ("should work for a value of 5", function() {
-        Test.assertSimilar(fibsFizzBuzz(5),[ 1, 1, 2, 'Fizz', 'Buzz' ])
-    });
+        console.log(fibsFizzBuzz(5),[ 1, 1, 2, 'Fizz', 'Buzz' ])
     
-    it ("should work for a value of 20", function() {
+    
         Test.assertSimilar(fibsFizzBuzz(20),[1,1,2,"Fizz","Buzz",8,13,"Fizz",34,"Buzz",89,"Fizz",233,377,"Buzz","Fizz",1597,2584,4181,"FizzBuzz"])
-    });
-});
+    
 
 
 
