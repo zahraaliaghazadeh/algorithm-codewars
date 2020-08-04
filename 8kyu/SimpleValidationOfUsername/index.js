443a7292b7cde1772dfdcbedfe686770c89f471f
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-04 13:57:41 -0700

    wip

--- 8kyu/SimpleValidationOfUsername/index.js
+++ 8kyu/SimpleValidationOfUsername/index.js
@@ -5,3 +5,21 @@
 // numbers,
 // underscore
 // Length should be between 4 and 16 characters (both included).
+
+function validateUsr(username) {
+    res =  /^[a-z\d_]{4,16}$/.test(username) 
+    return res
+  }
+
+
+    console.log(validateUsr('asddsa'), true);
+    console.log(validateUsr('a'), false);
+    console.log(validateUsr('Hass'), false);
+    console.log(validateUsr('Hasd_12assssssasasasasasaasasasasas'), false);
+    console.log(validateUsr(''), false);
+    console.log(validateUsr('____'), true);
+    console.log(validateUsr('012'), false);
+    console.log(validateUsr('p1pp1'), true);
+    console.log(validateUsr('asd43 34'), false);
+    console.log(validateUsr('asd43_34'), true);
+    
