bb2d20546e72ab7bace8cb5693e34fcadaffe519
Shaidee Alingcastre <shaidee@alingcastre.net>
2020-08-07 10:08:39 -0700

    edited comments

--- 7kyu/MoneyMoney/index.js
+++ 7kyu/MoneyMoney/index.js
@@ -1,7 +1,9 @@
 /* 7kyu
-Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
+Mr. Scrooge has a sum of money 'P' that he wants to invest. Before he does, he wants to know how many years 
+'Y' this sum 'P' has to be kept in the bank in order for it to amount to a desired sum of money 'D'.
 
-The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for the year the new sum is re-invested.
+The sum is kept for 'Y' years in the bank where interest 'I' is paid yearly. After paying taxes 'T' for 
+the year the new sum is re-invested.
 
 Note to Tax: not the invested principal is taxed, but only the year's accrued interest
 
@@ -20,9 +22,12 @@ After 3rd Year -->
   P = 1128.30
 Thus Mr. Scrooge has to wait for 3 years for the initial pricipal to ammount to the desired sum.
 
-Your task is to complete the method provided and return the number of years 'Y' as a whole in order for Mr. Scrooge to get the desired sum.
+Your task is to complete the method provided and return the number of years 'Y' as a whole in order for 
+Mr. Scrooge to get the desired sum.
 
-Assumption: Assume that Desired Principal 'D' is always greater than the initial principal. However it is best to take into consideration that if Desired Principal 'D' is equal to Principal 'P' this should return 0 Years.
+Assumption: Assume that Desired Principal 'D' is always greater than the initial principal. However it 
+is best to take into consideration that if Desired Principal 'D' is equal to Principal 'P' this should 
+return 0 Years.
 */
 
 function calculateYears(principal, interest, tax, desired) {
