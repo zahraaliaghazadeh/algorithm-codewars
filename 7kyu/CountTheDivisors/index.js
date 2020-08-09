547bc85fa107f835a1ebc29c45340952e06209ed
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-08 23:40:33 -0700

    wip

--- 7kyu/CountTheDivisors/index.js
+++ 7kyu/CountTheDivisors/index.js
@@ -20,3 +20,12 @@ function getDivisorsCnt(n){
   }
   return count
 }
+
+
+console.log(getDivisorsCnt(1) , 1);
+console.log(getDivisorsCnt(10) , 4);
+console.log(getDivisorsCnt(11) , 2);
+console.log(getDivisorsCnt(54) , 8);
+
+// other solution
+// function getDivisorsCnt(n){c=0; for (i=1;i<=(n>>1)+1;i++) if (n%i==0) c++; return n==1?1:c+1}
