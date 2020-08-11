9245984f24e4b83f7535c336be9ebc83452749f8
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-11 11:14:05 -0700

    driving school series

--- 7kyu/ReverseWords/index.js
+++ 7kyu/ReverseWords/index.js
@@ -1,4 +1,9 @@
 // 7kyu
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmax
+
 
 
 function reverseWords(str) {
@@ -28,7 +33,3 @@ function reverseWords(str) {
 //   function reverseWords(str) {
 //     return str.split(' ').map( str => str.split('').reverse().join('') ).join(' ');
 //   }
-
-// other solution
-// const reverseWords = str =>
-//   str.replace(/\S+/g, val => [...val].reverse().join(``));
