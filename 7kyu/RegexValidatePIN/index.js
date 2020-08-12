305817640705b8e4f7913e36fe9d84054c7145f1
Shaidee Alingcastre <shaidee@alingcastre.net>
2020-08-12 10:42:33 -0700

    added solution

--- 7kyu/RegexValidatePIN/index.js
+++ 7kyu/RegexValidatePIN/index.js
@@ -9,28 +9,45 @@
 // validatePIN("12345") === false
 // validatePIN("a234") === false
 
+function validatePIN(pin) {
+  return /^(\d{4}|\d{6})$/.test(pin);
+}
 
+console.log(validatePIN("1"))         // false
+console.log(validatePIN("12"))        // false
+console.log(validatePIN("123"))       // false
+console.log(validatePIN("12345"))     // false
+console.log(validatePIN("1234567"))   // false
+console.log(validatePIN("-1234"))     // false
+console.log(validatePIN("1.234"))     // false
+console.log(validatePIN("-1.234"))    // false
+console.log(validatePIN("00000000"))  // false
 
-Test.describe("validatePIN", function() {
-    Test.it("should return False for pins with length other than 4 or 6", function() {
-      Test.assertEquals(validatePIN("1"),false, "Wrong output for '1'")
-      Test.assertEquals(validatePIN("12"),false, "Wrong output for '12'")
-      Test.assertEquals(validatePIN("123"),false, "Wrong output for '123'")
-      Test.assertEquals(validatePIN("12345"),false, "Wrong output for '12345'")
-      Test.assertEquals(validatePIN("1234567"),false, "Wrong output for '1234567'")
-      Test.assertEquals(validatePIN("-1234"),false, "Wrong output for '-1234'")
-      Test.assertEquals(validatePIN("1.234"),false, "Wrong output for '1.234'")
-      Test.assertEquals(validatePIN("-1.234"),false, "Wrong output for '-1.234'")
-      Test.assertEquals(validatePIN("00000000"),false, "Wrong output for '00000000'")
-    });
     
-      console.log(validatePIN("a234"),false, "Wrong output for 'a234'")
-      console.log(validatePIN(".234"),false, "Wrong output for '.234'")
-      console.log(validatePIN("1234"),true, "Wrong output for '1234'");
-      console.log(validatePIN("0000"),true, "Wrong output for '0000'");
-      console.log(validatePIN("1111"),true, "Wrong output for '1111'");
-      console.log(validatePIN("123456"),true, "Wrong output for '123456'");
-      console.log(validatePIN("098765"),true, "Wrong output for '098765'");
-      console.log(validatePIN("000000"),true, "Wrong output for '000000'");
-      console.log(validatePIN("123456"),true, "Wrong output for '123456'");
-      console.log(validatePIN("090909"),true, "Wrong output for '090909'");
+console.log(validatePIN("a234"))      // false
+console.log(validatePIN(".234"))      // false
+console.log(validatePIN("1234"))      // true
+console.log(validatePIN("0000"))      // true
+console.log(validatePIN("1111"))      // true
+console.log(validatePIN("123456"))    // true
+console.log(validatePIN("098765"))    // true
+console.log(validatePIN("000000"))    // true
+console.log(validatePIN("123456"))    // true
+console.log(validatePIN("090909"))    // true
+
+/*=========== other solutions ==============
+
+function validatePIN (pin) {
+  
+  var pinlen = pin.length;
+  var isCorrectLength = (pinlen == 4 || pinlen == 6);
+  var hasOnlyNumbers = pin.match(/^\d+$/);
+    
+  if(isCorrectLength && hasOnlyNumbers){
+    return true;
+  }
+  
+  return false;
+
+}
+*/
