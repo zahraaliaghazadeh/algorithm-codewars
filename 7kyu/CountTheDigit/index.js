aa599a4d5581848de7c0769660be10054f36727c
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-08-12 09:06:19 -0700

    other solutions to count the d

--- 7kyu/CountTheDigit/index.js
+++ 7kyu/CountTheDigit/index.js
@@ -11,3 +11,60 @@
 // 1, 4, 9, 10, 11, 12, 13, 14, 19, 21 which squared are 1, 16, 81, 100, 121, 144, 169, 196, 361, 441
 // so there are 11 digits `1` for the squares of numbers between 0 and 25.
 // Note that 121 has twice the digit 1.
+
+function nbDig(n, d) {
+    // your code
+  let arr =[]
+  for ( let i = 0 ; i <=n ; i++){
+    arr.push(i**2)
+  }
+  let array = arr.join("")
+  var count = 0
+  for (let j=0 ; j<array.length; j++){
+    if (array[j].includes(d))
+      count ++
+  }
+  return count
+}
+
+        console.log(nbDig(5750, 0), 4700)
+        console.log(nbDig(11011, 2), 9481)
+        console.log(nbDig(12224, 8), 7733)
+        console.log(nbDig(11549, 1), 11905)
+
+
+        // other solution
+        // function nbDig(n, d) {
+        //     var res=0;
+        //         for (var g=0;g<=n;g++){
+        //           var square=(g*g+"").split("");
+        //           square.forEach((s)=>s==d?res++:null)
+        //         }return res;
+        //     }
+
+        // other solution
+            // nbDig = (n, d) => Array.from(Array(n + 1), (e, i) => i * i).join('').match(new RegExp(d, 'g')).length;
+
+            // other solution
+            // function nbDig(n, d) {
+            //     return [...Array(n+1).keys()].map((i) => {
+            //       return i * i;
+            //     })
+            //     .join('')
+            //     .split('')
+            //     .filter((i) => {
+            //       return i == d;
+            //     }).length;
+            //   }
+
+            // other solution
+            //   function nbDig(n, d) {
+            //     return Array.from({length: n+1}, (_,i) => Math.pow(i,2)).join('').match(new RegExp(d,'g')).length;
+            // }
+
+            // other solution
+            // function nbDig(n, d) {
+            //     let but = RegExp("[^"+d+"]","g"), tot = 0;
+            //     while( n >-1 ) tot += (''+(n*n--)).replace(but,'').length;
+            //     return tot
+            //   }
