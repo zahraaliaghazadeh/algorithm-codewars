5ae1fe84a2f23fa1fdbd30ede511b014b9470c11
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-09-18 11:07:13 -0700

    find the middle element

---
+++ 7kyu/FindTheMiddleElement/index.js
@@ -0,0 +1,51 @@
+// 7kyu
+// As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+// The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+// For example:
+
+// gimme([2, 3, 1]) => 0
+// 2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+// Another example (just to make sure it is clear):
+
+// gimme([5, 10, 14]) => 1
+// 10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+
+
+var gimme = function (inputArray) {
+
+    if ( (inputArray[0] > inputArray[1] && inputArray[0] < inputArray[2]) ||
+      (inputArray[0] > inputArray[2] && inputArray[0] < inputArray[1]) ){
+//       return indexOf(inputArray[0])
+return 0
+    }
+  else if ( (inputArray[2] > inputArray[1] && inputArray[2] < inputArray[0]) ||
+      (inputArray[2] > inputArray[0] && inputArray[2] < inputArray[1]) ){
+    
+//       return indexOf(inputArray[2])
+    return 2
+    
+  }else if ( (inputArray[1] > inputArray[2] && inputArray[1] < inputArray[0]) ||
+      (inputArray[1] > inputArray[0] && inputArray[1] < inputArray[2]) ){
+    
+//       return indexOf(inputArray[1])
+    return 1
+    
+  }
+  
+
+};
+
+
+
+console.log(gimme([2, 3, 1]), 0, 'Finds the index of middle element');
+console.log(gimme([5, 10, 14]), 1, 'Finds the index of middle element')
+
+
+
+// Other solution
+// function gimme(a) {
+//     return a.indexOf(a.concat().sort(function(a, b) { return a - b })[1])
+//   }
