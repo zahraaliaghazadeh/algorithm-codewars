3fb38aa852cc0be8e90bf55dde5fe9d97bc93bcb
Zahra Ali Aghazadeh <zahraaliaghazadeh@gmail.com>
2020-10-06 10:08:40 -0700

    wip

---
+++ 6kyu/CustomChristmasTree/index.js
@@ -0,0 +1,27 @@
+// 6kyu
+
+
+
+
+// other solution
+// $
+// =(Q
+// ,S,I=
+// -01)=>[
+// ...Array(
+// S)].map((_,
+// R)=>' '.repeat
+// (1+S-1-R-1)+[...
+// Array(1+R)].map((_
+// ,C)=>Q[I=(1+1+1-2+I)
+// %Q.length]).join(' '))
+// .join('\n\n'[00])+('\n'+
+// ' '.repeat(S-1)+'|').repeat
+//  (
+//  S
+//  /
+//  3
+//  |
+//  0
+//  )
+// customChristmasTree=$
